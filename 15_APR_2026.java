d8702b9a09ddc59b84f0bfac0794c79cb017038a
“[Aman <amanverma0202004@gmail.com>
2026-04-15 20:07:34 +0530

    Solved bulb switcher

---
+++ 15_APR_2026.java
@@ -0,0 +1,11 @@
+//319.  Bulb Switcher
+class Solution {
+    public int bulbSwitch(int n) {
+        int i=1;
+        while(i*i<=n){
+            System.out.println(i);
+            i++;
+        }
+       return(i-1);
+    }
+}
